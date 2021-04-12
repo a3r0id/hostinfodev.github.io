ed145b85755e91f2ae5064cd934be6e1f5772cb0
CyberCreatureSec <50676042+cybercreaturesec@users.noreply.github.com>
2021-04-12 03:27:10 -0700

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -7,17 +7,25 @@ var boolMusicPlaying = false;
 $("#canvas").focus();
 $("#canvas").click();
 
+
 const tryPlay = setInterval(()=>{
     if (state.mouse.x != 0 && state.mouse.y != 0){
         try{
             music.play();
             clearInterval(tryPlay);
             boolMusicPlaying = true;
-        } catch(e) {}
+        } catch(e) {
+            if (prompt("This page contains music, would you like to enjoy the complete experience?")){
+                music.play();
+            }
+        }
     }
 }, 10);
 
+alert("Headphones On!");
 tryPlay;
+
+music.play();
 
 $('#music').on('click', () => {
     if (boolMusicPlaying) {
